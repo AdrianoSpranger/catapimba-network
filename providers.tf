af2b0cb96f4177a3fbc688f8bd8418796e1a1d2e
Adriano da Silva Lungo <adriano.lungo@smartbank.local>
2022-09-30 14:57:03 -0300

    Adicionando catapimba

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.32.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  #region = "us-east-2"
+  #access_key = "AKIAUOCAD7AZGE6WDPOR"
+  #secret_key = "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"
+}
